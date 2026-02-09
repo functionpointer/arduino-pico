dc8ea61d4dfc0ba1f99b4b83d9c0afc47dd7fa1a
functionpointer <suspendfunction@gmail.com>
2026-02-09 15:55:46 +0100

    ClientContext.h: Prevent double free for freeRTOS
    Previous LWIPMutex solution only worked for core

--- cores/rp2040/lwip_wrap.h
+++ cores/rp2040/lwip_wrap.h
@@ -20,6 +20,7 @@
 
 #pragma once
 
+#include <functional>
 #include <Arduino.h>
 #include <pico/cyw43_arch.h>
 #include <lwip/pbuf.h>
@@ -167,6 +168,7 @@ typedef enum {
 #endif
 
     __callback = 10000,
+    __function,
 } __lwip_op;
 
 // Set up a local request buffer and call this to add to lwip work queue.  Will only return once lwip operation completed
@@ -618,4 +620,7 @@ extern void lwip_callback(void (*cb)(void *), void *cbData, __callback_req *buff
 
 #ifdef __cplusplus
 };
+
+extern void lwip_callback(std::function<void(void)> cb);
+
 #endif
